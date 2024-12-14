df5297546e5169c996534287e5a1c89b6b003212
Luiz Felipe Stangarlin <luizfelipestang@gmail.com>
2024-12-14 14:29:22 -0600

    fix: CJS modules being imported from ESM context

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,17 @@
 import type { Options } from 'tsup'
+import { commonjs } from "@hyrious/esbuild-plugin-commonjs"
+import module from 'node:module'
 
-export const tsup:Options = {
-	dts: true,
+export const tsup: Options = {
+  dts: true,
   splitting: true,
   sourcemap: true,
-	minify: true,
-	format: ['cjs', 'esm'],
+  minify: false,
+  format: ['cjs', 'esm'],
   entryPoints: ['src/index.ts'],
+  esbuildPlugins: [
+    commonjs({
+      ignore: (path) => !(module.builtinModules.includes(path) || path.startsWith('node:'))
+    }),
+  ],
 }
